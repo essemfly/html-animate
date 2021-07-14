5850b9bfef9724af1641816bb5a8dbc241d18411
Seokmin Lee <essemfly@gmail.com>
2021-07-14 17:27:24 +0900

    Add borders

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const card = document.querySelector('.card__inner');
+const cardInner = document.querySelector('.card__inner');
+const card = document.querySelector('.card');
 
 card.addEventListener('click', function() {
-    card.classList.toggle('is-flipped');
+    cardInner.classList.toggle('is-flipped');
+    card.classList.toggle('card_changer');
+    document.body.classList.toggle("body_changer");
 });
-
