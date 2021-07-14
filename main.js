ca6e8f299479c3b03f68a71fcd9dabef4c2d5347
Seokmin Lee <essemfly@gmail.com>
2021-07-14 18:21:12 +0900

    Test on making box

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const cardInner = document.querySelector('.card__inner');
 const card = document.querySelector('.card');
+const cube = document.querySelector('.cube');
 
 card.addEventListener('click', function() {
     cardInner.classList.toggle('is-flipped');
     card.classList.toggle('card_changer');
     document.body.classList.toggle("body_changer");
+    cube.classList.toggle("show-right")
 });
